230d2f2f6939a8d90330ff700836d86e7eb03cba
leonardobonato <leoleobonaato@gmail.com>
2023-06-20 21:30:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,1 @@
+<footer> <p>Desenvolvido em Pelotas, RS</p> <p>Por Leonardo Bonato</p> <img src="" alt="Logo do LinkedIn" /> <img src="" alt="Logo do Instagram" /> <img src="" alt="Link app store apple" /> <img src="" alt="Link google play" /> </footer>
