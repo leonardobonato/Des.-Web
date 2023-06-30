79f79958777b417fb520c25de978042f1a60a976
leonardobonato <leoleobonaato@gmail.com>
2023-06-30 18:20:52 -0300

    commit com alterações na aparência do site

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-<footer> <p>Desenvolvido em Pelotas, RS</p> <p>Por Leonardo Bonato</p> <img src="" alt="Logo do LinkedIn" /> <img src="" alt="Logo do Instagram" /> <img src="" alt="Link app store apple" /> <img src="" alt="Link google play" /> </footer>
+const pontualidade = document.querySelector('nav-link')
+
+function pontualidades() {
+    
+}
